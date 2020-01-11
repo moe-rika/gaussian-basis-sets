f81b6807c5bf15703fe2e18ee9e2672444c42221
DiazoPlus <wwwxxttii@gmail.com>
2020-01-12 00:07:28 +0800

    C

--- gaussian_basis_sets/iter.hpp
+++ gaussian_basis_sets/iter.hpp
@@ -1,79 +1,9 @@
 #include <initializer_list>
 #include <assert.h>
 #include <iostream>
-//#include <string>
-#include <set>
+#include <tuple>
 #include <array>
 
-//template <unsigned int N>
-//class MyIter {
-//public:
-//	MyIter(const std::initializer_list<int>& _init)
-//	{
-//		assert(N > 0);
-//		assert(_init.size() == N);
-//		assert(*(_init.end() - 1) != 0);
-//		int index = 0;
-//		bool flag = true;
-//		for (auto &a : _init)
-//		{
-//			if (a != 0)
-//				flag = false;
-//			assert(a >= 0);
-//			cnt[index] = a - 1;
-//			it[index] = a - 1;
-//			index++;
-//		}
-//		m_is_zero = flag;
-//	}
-//	const bool is_zero() const
-//	{
-//		return m_is_zero;
-//	}
-//	const MyIter& operator --()
-//	{
-//		dec(0);
-//		return *this;
-//	}
-//	const int& operator[](int i) const
-//	{
-//		return it[i];
-//	}
-//private:
-//	int cnt[N];
-//	int it[N];
-//	bool m_is_zero;
-//	void dec(int i)
-//	{
-//		if (!m_is_zero)
-//		{
-//			if (i == N - 1)
-//			{
-//				if (it[i] == 1)
-//				{
-//					it[i] = 0;
-//					m_is_zero = true;
-//				}
-//				else
-//				{
-//					it[i]--;
-//				}
-//			}
-//			else
-//			{
-//				if (it[i] != 0)
-//				{
-//					it[i]--;
-//				}
-//				else
-//				{
-//					it[i] = cnt[i];
-//					dec(++i);
-//				}
-//			}
-//		}
-//	}
-//};
 
 //provide mathematica style iter
 template <unsigned int N>
@@ -107,13 +37,7 @@ struct MyRangeIter{
 			step[i] = std::get<3>(a);
 		}
 	}
-	std::array<int*, N> ptr;
-	std::array<int, N> start;
-	std::array<int, N> end;
-	std::array<int, N> step;
-	std::array<int, N> current;
-private:
-	bool finish = false;
+
 	void next()
 	{
 		if (!finish)
@@ -121,6 +45,16 @@ private:
 			one_index_next(0);
 		}
 	}
+
+private:
+	std::array<int*, N> ptr;
+	std::array<int, N> start;
+	std::array<int, N> end;
+	std::array<int, N> step;
+	std::array<int, N> current;
+
+	bool finish = false;
+
 	void one_index_next(int n)
 	{
 		if (n == N)
@@ -144,30 +78,3 @@ private:
 	}
 };
 
-//int main()
-//{
-//	for (MyIter<3> iter = { 2,3,4 }; !iter.is_zero(); --iter)
-//	{
-//		std::cout << iter[0] << " " << iter[1] << " " << iter[2] << std::endl;
-//	}
-//	return 0;
-//}
-
-//
-//class SimpleMultiThreadTter
-//{
-//public:
-//	SimpleMultiThreadTter();
-//	~SimpleMultiThreadTter();
-//	class index
-//private:
-//
-//};
-//
-//SimpleMultiThreadTter::SimpleMultiThreadTter()
-//{
-//}
-//
-//SimpleMultiThreadTter::~SimpleMultiThreadTter()
-//{
-//}
